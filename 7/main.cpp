ccb362c4267c25cb644c482583a941298ae82af3
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2026-01-12 18:47:54 +0100

    Add files via upload

--- 7/main.cpp
+++ 7/main.cpp
@@ -1,81 +1,43 @@
-#define RAYGUI_IMPLEMENTATION
 #include<raylib.h>
 #include<iostream>
-#include"raygui.h"
-using namespace std;
 
-//#define FRAMERATE 60
-//change it into a varibale 
-#define WIDTH 720
-#define HEIGHT 1080
-
-enum  Status{
-  HOMEPAGE,
-  MAIN,
-  SETTINGS,
-};
-
-void show_fps_in_screen()
-{
-    DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, RED);
+void move_camera(Camera3D &camera){
+  if (IsKeyDown(KEY_W)) camera.position.z -= 0.2f;
+  if (IsKeyDown(KEY_S)) camera.position.z += 0.2f;
+  if (IsKeyDown(KEY_A)) camera.position.x -= 0.2f;
+  if (IsKeyDown(KEY_D)) camera.position.x += 0.2f;
+  if (IsKeyDown(KEY_Q)) camera.position.y -= 0.2f;
+  if (IsKeyDown(KEY_E)) camera.position.y += 0.2f;
 }
-//float FRAMERATE = 60.0;
 
-Vector2 mouse_position = {0};
+
+
+using namespace std ;
+
 int main(){
- 
-int  FRAMERATE = 60;
-int screen_w = GetScreenWidth();
-int screen_h = GetScreenHeight();
+  InitWindow(1024,800,"app");
+  SetTargetFPS(60);
 
-  InitWindow(screen_w,screen_h ,"moha app");
-  SetTargetFPS(FRAMERATE);
-
-  bool clicked =  false ;
-  Status status = HOMEPAGE;
-
-while (!WindowShouldClose())
-{
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    if (status == HOMEPAGE)
-    {
-        if (GuiButton({(screen_w - 120)/ 2.0f, (screen_h - 30 ) / 2.0f, 120, 30}, "Start"))
-        {
-            status = MAIN;
-        }
-        if(GuiButton({100,777 ,120,30},"SETTINGS")){
-          status = SETTINGS;
-        }
-     }
-    else if(status == MAIN)
-    {
-      if(GuiButton({WIDTH/2,HEIGHT/2 -240,120,30}, "return")){
-        status = HOMEPAGE;
-      }
-        DrawText("MAIN SCREEN", 50, 100, 30, GREEN);
+   Model model = LoadModel("assets/robot.glb");
+   Camera3D camera = { {3,3,3}, {0,0,0}, {0,1,0}, 45, 0 };
+   if (model.meshCount == 0) {
+        model = LoadModelFromMesh(GenMeshCube(1.0f, 2.0f, 1.0f));
     }
-    else if(status == SETTINGS)
-    {
-      if(GuiButton({100,100,120,30},"60 fps")){
-        FRAMERATE = 60;
-      }
-      else if(GuiButton({100,150,120,30},"120 fps")){
-        FRAMERATE = 120;
-        
-      }
-      else if (GuiButton({100,200,120,30},"240 fps")){
-        FRAMERATE = 240;
-      }
-      SetTargetFPS(FRAMERATE);
-      show_fps_in_screen();
-      
-    }
+   DisableCursor();
+   //CameraMode(camera, CAMERA_FREE);
+   // wont work 
+  // SetCameraMode(camera, CAMERA_FREE);
+   while(!WindowShouldClose()){
+     BeginDrawing();
+      move_camera(camera);
+      ClearBackground(RAYWHITE);
+        BeginMode3D(camera);
+           DrawModel(model, (Vector3){0, 0, 0}, 1.0f, WHITE);
+           DrawGrid(10, 1.0f);
+        EndMode3D();
+      EndDrawing();
+   }
+   UnloadModel(model);
 
-    EndDrawing();
-}
   CloseWindow();
-
-  return 0;
 }
